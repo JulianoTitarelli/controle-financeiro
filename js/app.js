0683518f66d0eb96e1768f66d29e054d43127e8d
JulianoTitarelli <julianotitarelli@outlook.com>
2026-09-26 09:07:00 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
 import { db } from "./firebase.js";
 
+import {
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+
 console.log("Firebase conectado!");
 console.log(db);
+
+
+const botaoTeste = document.getElementById("testeFirebase");
+
+
+botaoTeste.addEventListener("click", async () => {
+
+    try {
+
+        await addDoc(
+            collection(db, "entradas"),
+            {
+                descricao: "Teste do sistema",
+                valor: 10,
+                data: new Date(),
+                status: "recebido"
+            }
+        );
+
+        alert("Entrada salva com sucesso!");
+
+    } catch (erro) {
+
+        console.error("Erro ao salvar:", erro);
+
+        alert("Erro ao salvar no Firebase.");
+
+    }
+
+});
