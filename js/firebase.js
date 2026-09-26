34112e271a42b6e23e8e7b9b51726d66a5ab339e
JulianoTitarelli <julianotitarelli@outlook.com>
2026-09-26 10:41:24 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,56 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+```javascript
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDw6y1sqRa6P-BAY2KE22KkNXz7GYg3bEo",
-    authDomain: "controle-financeiro-f6df9.firebaseapp.com",
-    projectId: "controle-financeiro-f6df9",
-    storageBucket: "controle-financeiro-f6df9.firebasestorage.app",
-    messagingSenderId: "829663947104",
-    appId: "1:829663947104:web:e114bd130737161ce65605"
+
+    apiKey:
+        "AIzaSyDw6y1sqRa6P-BAY2KE22KkNXz7GYg3bEo",
+
+    authDomain:
+        "controle-financeiro-f6df9.firebaseapp.com",
+
+    projectId:
+        "controle-financeiro-f6df9",
+
+    storageBucket:
+        "controle-financeiro-f6df9.firebasestorage.app",
+
+    messagingSenderId:
+        "829663947104",
+
+    appId:
+        "1:829663947104:web:e114bd130737161ce65605"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
+const app =
+    initializeApp(
+        firebaseConfig
+    );
 
-export { db };
+
+const db =
+    getFirestore(app);
+
+
+const auth =
+    getAuth(app);
+
+
+export {
+    db,
+    auth
+};
+```
